d2ea03959176390303e96e55f40721057cd533ba
Leonardo Appio <leonardo.appio@gmail.com>
2021-07-03 13:01:47 -0300

    Implementada a jogada especial de promocao

--- jogo-xadrez/xadrez/PartidaDeXadrez.cs
+++ jogo-xadrez/xadrez/PartidaDeXadrez.cs
@@ -178,6 +178,22 @@ namespace xadrez
                 throw new ExcecoesTabuleiro("Você não pode se colocar em xeque");
             }
 
+            Peca p = tab.getPeca(destino);
+
+            // jogada especial - promocao
+            if(p is Peao)
+            {
+                if((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -197,7 +213,7 @@ namespace xadrez
                 mudaJogador();
             }
 
-            Peca p = tab.getPeca(destino);
+            
 
             // jogada especial en passant
             if(p is Peao && (destino.linha == origem.linha - 2 || destino.linha == destino.linha + 2))
